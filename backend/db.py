b7b21c027e1094f8fefd00801fee53053c6dd698
Meghana KR <meghanakr2004@gmail.com>
2026-03-27 13:24:34 +0530

    added mongo URI

--- backend/db.py
+++ backend/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["ai_interview"]
 
